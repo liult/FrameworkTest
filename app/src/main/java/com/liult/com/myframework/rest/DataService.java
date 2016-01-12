1559b2b576f2b03f24f441a0817ade5461a11cac
liuletao <letao.liu@dmall.com>
2016-01-12 19:03:36 +0800

    add getui and retrofit and rxjava

--- app/src/main/java/com/liult/com/myframework/rest/DataService.java
+++ app/src/main/java/com/liult/com/myframework/rest/DataService.java
@@ -12,7 +12,7 @@ public class DataService {
     public static final String RELEASE_ENDPOINT = "http://api.1mxian.com/";
 
     private static DataService sInstance;
-//http://www.th7.cn/Program/Android/201510/603107.shtml
+//http://www.th7.cn/Program/Android/201510/603107.shtml  http://square.github.io/retrofit/
     public DataService() {
         //经过测试: baseUrl必须设置,如果 声明接口时@GET使用了完整的url路径,那么baseUrl就会被忽略,否则就是拼接url
         Retrofit builder = new Retrofit.Builder()//在这里可以添加 Gson转换器等;
